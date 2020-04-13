a3aaeff0b30295821b9203b1e32ae5d9fb9bef48
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-13 22:25:57 +0900

    完成！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.order(created_at: :desc)
   end
+
+  def new
+    @posts = Posts.new
+  end
+
+  def creat
+    @posts = Posts.new
+    if @posts.save
+      flash
+
 end
