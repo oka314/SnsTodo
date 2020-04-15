c3c8055f162c7c5c3c8b14d96d0774a71c529c3e
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-15 19:43:51 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,22 @@ class PostsController < ApplicationController
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
+   
   end
 
-  def creat
-    @posts = Post.create(post_params)
-    if @posts.save
+  def create
+    @post = Post.create(post_params)  
+    if @post.save
       redirect_to user_path
     else
       render :new
       
     end
   end
-  private post_params
+
+  private 
+  def post_params
     params.require(:post).permit(:explain)
+  end
 end
