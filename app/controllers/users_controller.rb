a85fd62c9003f296262624de0abe1b3e4caed6d3
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-16 21:23:33 +0900

    投稿者マイページへのリンク

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
   def show
    @name = current_user.name
    @posts = current_user.posts
+   @user = User.find(params[:id])
+   @post = @user.posts
   end
 end
