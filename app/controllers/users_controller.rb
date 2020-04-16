616bbcbaca7a85efec4400dac5fbdfb4f73b69f9
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-16 22:33:11 +0900

    完成！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
  
   def show
-  #  @name = current_user.name
-  #  @posts = current_user.posts
+   @name = current_user.name
+   @posts = current_user.posts
    @user = User.find(params[:id])
    @post = @user.posts
   end
