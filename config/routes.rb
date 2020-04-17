17fe76e3b1368a9bb805fa9062e79553290ffdb7
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-17 22:46:12 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
  
   devise_for :users
   root 'top#index'
-  resources :posts, only:[:index,:new,:create,:show,:destroy]
+  resources :posts, only:[:index,:new,:create,:show,:destroy]　do
+    resources :likes, only:[:create,:destroy]
+  end
   resources :users, only: [:show]
  
 end
