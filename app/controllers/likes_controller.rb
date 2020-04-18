61b5dd2ad4dd5fb6860fcb6a540cda4a10c0e73e
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-18 11:03:43 +0900

    いいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_post,only:[:create,:destroy]
   def create
-    @like
+    @like = current_user.likes.create(like_params)
+    redirect_to posts_path,notice:'いいねしました！'
   end
 
   def destroy
+    @like = Like.find_by(like_params, user_id: current_user.id)
+    @like.destroy
+    redirect_to posts_path,notice: 'いいねを削除しました。'
+  end
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+  def like_params
+    params.permit(:post_id)
   end
 end
