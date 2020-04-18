e2309fb1a79a45e009c9ba92eeb3a43ceb0d546e
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-18 15:16:45 +0900

    途中経過

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,20 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user
-  def create
-    @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
-    if @like.save
-      redirect_to posts_path,notice:'いいねしました！'
-    else
-      redirect_to posts_path,notice:'いいね出来ませんでした！'
-    end
+  before_action :set_variables
 
-    def destroy            
-      @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])            
-      @like.destroy            
-      redirect_to posts_path,notice:'いいねを外しました'           
-   end
-  
-
+  def like
+    like = current_user.likes.new(post_id: @post.id)
+    like.save
   end
+
+  def unlike
+    like = current_user.likes.find_by(post_id: @post.id)
+    like.destroy
+  end
+
+  private
+
+  def set_variables
+    @post = Post.find(params[:post_id])
+    @id_name = "#like-link-#{@post.id}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
  
-  get 'likes/create'
-  get 'likes/destroy'
+
   devise_for :users
   root 'top#index'
   resources :posts, only:[:index,:new,:create,:show,:destroy] 
   resources :users, only: [:show]
-  post "likes/:post_id/create" => "likes#create"
- 
+  post   '/like/:product_id' => 'likes#like',   as: 'like'
+  delete '/like/:product_id' => 'likes#unlike', as: 'unlike'
+
 end
