1e5f181020d9f13915a9fdd0ac885295e5a7b2e6
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-18 17:38:50 +0900

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'top#index'
   resources :posts, only:[:index,:new,:create,:show,:destroy] 
   resources :users, only: [:show]
-  post "likes/:post_id/create" => "likes#create"
-  post "likes/:post_id/destroy" => "likes#destroy"
+  resources :searches, only: [:index]
+  #今後の自分の課題→いいね機能を絶対に実装する！
+  # post "likes/:post_id/create" => "likes#create"
+  # post "likes/:post_id/destroy" => "likes#destroy"
 end
