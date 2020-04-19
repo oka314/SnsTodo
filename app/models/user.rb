fc729c4bbeb761e27e96287f69095ef1910bbeb3
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-19 11:30:30 +0900

    日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name,presence: true,length:{maximum:15},format: { with: /\A[ぁ-んァ-ン一-龥]/}
 
  has_many :posts
  has_many :likes
