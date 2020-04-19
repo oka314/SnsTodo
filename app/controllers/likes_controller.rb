ef55aa4f9086456f1f91a1b1427198f58b2e4183
oka314 <61148080+oka314@users.noreply.github.com>
2020-04-19 19:05:57 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,21 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user
+#   before_action :authenticate_user
 
-  def create
-    @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
-    if @like.save
-      redirect_to posts_path,notice:'いいねしました！'
-    else
-      redirect_to posts_path,notice:'いいね出来ませんでした！'
-  end
+#   def create
+#     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
+#     if @like.save
+#       redirect_to posts_path,notice:'いいねしました！'
+#     else
+#       redirect_to posts_path,notice:'いいね出来ませんでした！'
+#   end
   
-  # destroyアクションを定義してください
- def destroy            
-    @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])            
-    if @like.destroy
-      redirect_to posts_path,notice:'いいねを外しました'
-    else
-      redirect_to posts_path,notice:'いいねを外せませんでした'      
- end
-  end
+#   # destroyアクションを定義してください
+#  def destroy            
+#     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])            
+#     if @like.destroy
+#       redirect_to posts_path,notice:'いいねを外しました'
+#     else
+#       redirect_to posts_path,notice:'いいねを外せませんでした'      
+#  end
+#   end
 end
